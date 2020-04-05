0f42457f9c425bee0e08c4322579695ecdec4f7e
kalash.fr@gmail.com <kalash.fr@gmail.com>
2020-04-05 09:21:54 +0200

    cours 61

--- webscrapery/main.py
+++ webscrapery/main.py
@@ -1,2 +1,10 @@
 from bs4 import BeautifulSoup
+import requests
 
+search = input("Enter search terms:")
+params = {"q" : search}
+
+r = requests.get("https://www.google.com/search", params=params)
+
+soup = BeautifulSoup(r.text, features="html.parser")
+print(soup.prettify())
