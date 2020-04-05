f6e24ddde53eab4364e3a2caa62d5d7ea36cb347
kalash.fr@gmail.com <kalash.fr@gmail.com>
2020-04-05 14:12:25 +0200

    Scrap on page of html pages

--- webscrapery/main.py
+++ webscrapery/main.py
@@ -4,7 +4,29 @@ import requests
 search = input("Enter search terms:")
 params = {"q" : search}
 
-r = requests.get("https://www.google.com/search", params=params)
+gr = requests.get("https://www.google.com/search", params=params)
 
-soup = BeautifulSoup(r.text, features="html.parser")
-print(soup.prettify())
+soup = BeautifulSoup(gr.text, "html.parser")
+
+resultDiv = soup.find_all('div', attrs = {'class' : 'ZINbbc'})
+
+links = []
+titles = []
+descriptions = []
+
+for r in resultDiv:
+    # Checks if each element is present, else, raise exception
+    try:
+        link = r.find('a', href = True)
+        link2 = r.find("a").attrs["href"]
+        title = r.find('div', attrs={'class':'vvjwJb'}).get_text()
+        description = r.find('div', attrs={'class':'s3v9rd'}).get_text()
+        
+        # Check to make sure everything is present before appending
+        if link != '' and title != '' and description != '': 
+            links.append(link['href'])
+            titles.append(title)
+            descriptions.append(description)
+    # Next loop if one element is not present
+    except:
+        continue
